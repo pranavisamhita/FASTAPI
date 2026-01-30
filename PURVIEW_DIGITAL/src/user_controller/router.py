4338acff72d2cfa677fdc59a6146b6d0c630d272
pranavisamhita <159760086+pranavisamhita@users.noreply.github.com>
2026-01-30 17:33:33 +0530

    Update router.py

--- PURVIEW_DIGITAL/src/user_controller/router.py
+++ PURVIEW_DIGITAL/src/user_controller/router.py
@@ -1,23 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from authentication.router import get_current_user
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-# GET
 @router.get("/")
-def get_users():
-    return {"message": "Get all users"}
+def get_users(current_user: str = Depends(get_current_user)):
+    return ["User 1", "User 2"]
 
-# POST
 @router.post("/")
-def create_user():
+def create_user(current_user: str = Depends(get_current_user)):
     return {"message": "User created"}
 
-# PUT
-@router.put("/{user_id}")
-def update_user(user_id: int):
-    return {"message": f"User {user_id} updated"}
-
-# DELETE
 @router.delete("/{user_id}")
-def delete_user(user_id: int):
+def delete_user(user_id: int, current_user: str = Depends(get_current_user)):
     return {"message": f"User {user_id} deleted"}
