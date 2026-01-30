96a0d87f587436477a6420d486fec489ecec6dda
pranavisamhita <159760086+pranavisamhita@users.noreply.github.com>
2026-01-30 17:33:11 +0530

    Update router.py

--- PURVIEW_DIGITAL/src/authentication/router.py
+++ PURVIEW_DIGITAL/src/authentication/router.py
@@ -1,7 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import Annotated
 
-router = APIRouter(prefix="/auth", tags=["authentication"])
+router = APIRouter(prefix="/auth", tags=["Authentication"])
 
-@router.post("/login")
-def login():
-    return {"message": "Login endpoint working"}
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+fake_user = {
+    "username": "admin",
+    "password": "admin123"
+}
+
+def authenticate_user(username: str, password: str):
+    return username == fake_user["username"] and password == fake_user["password"]
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    if token != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+        )
+    return token
+
+@router.post("/token")
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    if not authenticate_user(form_data.username, form_data.password):
+        raise HTTPException(status_code=400, detail="Wrong username or password")
+
+    return {
+        "access_token": "admin",
+        "token_type": "bearer"
+    }
